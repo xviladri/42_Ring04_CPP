ac9b3b8470b045140a63040a65d6b523193d75c3
xviladri <xeniavv@LAPTOP-84L48V78.localdomain>
2025-11-25 17:06:07 +0100

    primero

---
+++ c01/PhoneBook.hpp
@@ -0,0 +1,17 @@
+#ifndef PHONEBOOK_HPP
+# define PHONEBOOK_HPP
+
+# include "Contact.hpp"
+
+class PhoneBook {
+    private:
+        Contact _contacts[8];
+        int     _index;
+    public:
+        PhoneBook();
+        ~PhoneBook();
+        void    addContact();
+        void    searchContact();
+};
+
+#endif
