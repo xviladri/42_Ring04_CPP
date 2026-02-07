3ed0c43d1751879b529a3ae63ba0c1876993f998
xviladri <xeniavv@LAPTOP-84L48V78.localdomain>
2026-02-07 18:40:24 +0100

    ex04

--- MODULE01/ex04/main.cpp
+++ MODULE01/ex04/main.cpp
@@ -1,37 +1,44 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <cstdlib> // for exit
+#include <cstdlib>//for exit
 
-void replace(std::string filename, std::string s1, std::string s2) {
+void replace(std::string filename, std::string s1, std::string s2) 
+{
     std::ifstream ifs(filename.c_str());
-    if (!ifs.is_open()) {
+
+    if (!ifs.is_open()) 
+    {
         std::cerr << "Error: could not open file " << filename << std::endl;
         std::exit(1);
     }
 
-    std::string content;
-    // Read file char by char to preserve exact formatting
+    std::string content;// Read file char by char to preserve exact formatting
     char c;
-    while (ifs.get(c)) {
+    while (ifs.get(c)) 
+    {
         content += c;
     }
     ifs.close();
 
-    if (s1.empty()) {
+    if (s1.empty()) 
+    {
         // If s1 is empty, we do nothing as we can't replace "nothing"
-    } else {
+    } 
+    else 
+    {
         size_t pos = 0;
-        while ((pos = content.find(s1, pos)) != std::string::npos) {
+        while ((pos = content.find(s1, pos)) != std::string::npos) 
+        {
             content.erase(pos, s1.length());
             content.insert(pos, s2);
             pos += s2.length();
         }
     }
-
     std::string outFilename = filename + ".replace";
     std::ofstream ofs(outFilename.c_str());
-    if (!ofs.is_open()) {
+    if (!ofs.is_open()) 
+    {
         std::cerr << "Error: could not create file " << outFilename << std::endl;
         std::exit(1);
     }
@@ -39,12 +46,13 @@ void replace(std::string filename, std::string s1, std::string s2) {
     ofs.close();
 }
 
-int main(int argc, char **argv) {
-    if (argc != 4) {
-        std::cerr << "Usage: ./sed_is_for_losers <filename> <s1> <s2>" << std::endl;
+int main(int argc, char **argv) 
+{
+    if (argc != 4) 
+    {
+        std::cerr << "Error: Usage: ./sed_is_for_losers <filename> <s1> <s2>" << std::endl;
         return (1);
     }
-    
     replace(argv[1], argv[2], argv[3]);
     return (0);
 }
